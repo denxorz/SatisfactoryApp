e7627e0470c13e9283dcbe1bec05d0363e7b87a9
Dennis Geldhof <dennis@geldhof.eu>
2025-12-27 13:31:45 +0100

    Convert CargoFlow table to datagrid

---
+++ SatisfactoryApp/Models/CargoFlowSummary.cs
@@ -0,0 +1,10 @@
+namespace SatisfactoryApp.Models;
+
+public class CargoFlowSummary
+{
+    public string CargoType { get; set; } = string.Empty;
+    public double Produced { get; set; }
+    public double Consumed { get; set; }
+    public double Available => Produced - Consumed;
+}
+
